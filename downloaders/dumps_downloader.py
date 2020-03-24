497bbf495f8886b09d1b43c5f3ceb2e5288110ff
Gleb Drozdov <gleb.drozdov@jetbrains.com>
2020-03-25 01:37:08 +0300

    format file to 99 chars in line limit

--- downloaders/dumps_downloader.py
+++ downloaders/dumps_downloader.py
@@ -10,7 +10,8 @@ from tqdm import tqdm
 def download_progress_hook(p_bar: tqdm) -> (int, int, Optional[int]):
     last_block = [0]
 
-    def update_to(block_num: int = 1, block_size: int = 1, total_size: Optional[int] = None) -> None:
+    def update_to(block_num: int = 1, block_size: int = 1,
+                  total_size: Optional[int] = None) -> None:
         if total_size not in (None, -1):
             p_bar.total = total_size
         p_bar.update((block_num - last_block[0]) * block_size)
@@ -32,7 +33,8 @@ def extract_archive_links(url: str) -> [str]:
 
 def process_archives(archive_links: [str], target_dir: str) -> None:
     os.makedirs(target_dir, exist_ok=True)
-    for archive_link in tqdm(desc="process archives", iterable=archive_links, total=len(archive_links)):
+    for archive_link in tqdm(desc="process archives", iterable=archive_links,
+                             total=len(archive_links)):
         if not archive_link.endswith("tar.gz"):
             continue
         tar_filename = os.path.basename(archive_link)
@@ -50,7 +52,8 @@ def process_archives(archive_links: [str], target_dir: str) -> None:
 def untar(tarfile_path: str, target_directory: str, remove_tarfile: bool = True) -> None:
     os.makedirs(target_directory, exist_ok=True)
     with tarfile.open(tarfile_path, "r:gz") as tar:
-        for member in tqdm(desc="extracting %s" % tarfile_path, iterable=tar.getmembers(), total=len(tar.getmembers())):
+        for member in tqdm(desc="extracting %s" % tarfile_path, iterable=tar.getmembers(),
+                           total=len(tar.getmembers())):
             tar.extract(path=target_directory, member=member)
     if remove_tarfile:
         os.remove(tarfile_path)
