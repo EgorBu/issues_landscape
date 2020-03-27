15d29483eed6aa977d3127e3edb0d43a2cb57d08
Gleb Drozdov <gleb.drozdov@jetbrains.com>
2020-03-27 14:05:32 +0300

    add reStructured docstrings

--- downloaders/dumps_downloader.py
+++ downloaders/dumps_downloader.py
@@ -1,3 +1,7 @@
+"""
+Download Github issues daily dumps from this page
+"http://ghtorrent-downloads.ewi.tudelft.nl/mongo-daily/" and store them into target directory.
+"""
 import tarfile
 import os
 import argparse
@@ -9,10 +13,22 @@ from tqdm import tqdm
 
 
 def download_progress_hook(p_bar: tqdm) -> (int, int, Optional[int]):
+    """
+    Wraps tqdm instance
+    :param p_bar: tqdm instance responible for showing download progress
+    :return: function update_to which updates tqdm state
+    """
     last_block = [0]
 
     def update_to(block_num: int = 1, block_size: int = 1,
                   total_size: Optional[int] = None) -> None:
+        """
+        Update tqdm state
+        :param block_num: Number of blocks transferred so far
+        :param block_size: Size of each block
+        :param total_size: Total size (in tqdm units)
+        :return: None
+        """
         if total_size not in (None, -1):
             p_bar.total = total_size
         p_bar.update((block_num - last_block[0]) * block_size)
@@ -22,6 +38,11 @@ def download_progress_hook(p_bar: tqdm) -> (int, int, Optional[int]):
 
 
 def extract_archive_links(url: str) -> [str]:
+    """
+    Extract tar files links from GHTorrent html page to list
+    :param url: url of page with daily dumps tars
+    :return: list of tar files links
+    """
     html_page = urllib.request.urlopen(url)
     soup = BeautifulSoup(html_page, features="html.parser")
     tar_files_links = []
@@ -33,6 +54,12 @@ def extract_archive_links(url: str) -> [str]:
 
 
 def process_archives(archive_links: [str], target_dir: str) -> None:
+    """
+    Download tar files and untar them to target directory
+    :param archive_links: list of tar files links
+    :param target_dir: target directory
+    :return: None
+    """
     os.makedirs(target_dir, exist_ok=True)
     for archive_link in tqdm(desc="process archives", iterable=archive_links,
                              total=len(archive_links)):
@@ -52,6 +79,13 @@ def process_archives(archive_links: [str], target_dir: str) -> None:
 
 
 def untar(tarfile_path: str, target_directory: str, remove_tarfile: bool = True) -> bool:
+    """
+    Untar tarfile to target directory and remove tarfile if it is necessary
+    :param tarfile_path: path of tarfile
+    :param target_directory: directory where files will be extracted
+    :param remove_tarfile: True if necessary to remove tarfile, otherwise - False
+    :return: True if untar process was successful, else - False
+    """
     os.makedirs(target_directory, exist_ok=True)
     with tarfile.open(tarfile_path, "r:gz") as tar:
         try:
@@ -66,6 +100,11 @@ def untar(tarfile_path: str, target_directory: str, remove_tarfile: bool = True)
 
 
 def remove_excess_files(directory: str) -> None:
+    """
+    Remove all files from directory, except for related to issues
+    :param directory: directory where excess files will be deleted
+    :return: None
+    """
     issue_related_files = {"issues.bson", "issue_comments.bson"}
     bson_files = os.listdir(directory)
     for file in bson_files:
@@ -73,7 +112,11 @@ def remove_excess_files(directory: str) -> None:
             os.remove(os.path.join(directory, file))
 
 
-def main():
+def main() -> None:
+    """
+    Download and untar Github daily dumps tar files to target_dir
+    :return: None
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--target-dir", required=True,
                         help="Directory to store downloaded tar files")
